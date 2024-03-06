d692528cde24972ef7bf3732a8875b5ba0b8e596
Sávio <saviopf22@gmail.com>
2024-03-06 01:20:01 -0300

    testes com manipulação de clipboard

--- src/app/components/calculator/calculator.component.cy.ts
+++ src/app/components/calculator/calculator.component.cy.ts
@@ -11,6 +11,25 @@ describe('CalculatorComponent tests', () => {
       cy.get('#input2').blur();
 
       cy.get('#total').should('have.text', '34');
+
+      cy.window().focus();
+
+      cy.get('#total').then(($value) => {
+        const text = $value.text();
+
+        console.log({ text });
+
+        cy.window().its('navigator.clipboard').invoke('writeText', text);
+      });
+
+      cy.window()
+        .its('navigator.clipboard')
+        .then((clip) => clip.readText())
+        .then((text) => {
+          console.log({ text });
+
+          cy.get('#total-response').type(text + ' test clipboard:)');
+        });
     });
   });
 });
